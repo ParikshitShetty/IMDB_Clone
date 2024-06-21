1b6621386bf446f2f5ce997a52c4eeefa77f3860
ParikshitShetty <parikshitshetty66@gmail.com>
2024-06-21 16:30:00 +0530

    first commit

---
+++ UI/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-screen min-h-screen bg-slate-900'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Users (
+  id INTEGER PRIMARY KEY,
+  user_name TEXT,
+  password TEXT
+);
+
+CREATE TABLE Actors (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  gender TEXT,
+  dob DATE,
+  bio TEXT
+);
+
+CREATE TABLE Producers (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  gender TEXT,
+  dob DATE,
+  bio TEXT
+);
+
+CREATE TABLE Movies (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  Year_Of_Release TEXT,
+  Plot TEXT,
+  poster BLOB,
+  producer_id INTEGER,
+  FOREIGN KEY (producer_id) REFERENCES Producers (id)
+);
+
+CREATE TABLE MoviesActors (
+  actor_id INTEGER,
+  movie_id INTEGER,
+  PRIMARY KEY (actor_id, movie_id),
+  FOREIGN KEY (actor_id) REFERENCES Actors (id),
+  FOREIGN KEY (movie_id) REFERENCES Movies (id)
+);
